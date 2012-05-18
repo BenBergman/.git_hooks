5b4f4b5adfb1338f3bef9b320ead26ef1c790915
Benjamin Bergman <ben.bergman@gmail.com>
2012-05-17 19:04:51 -0500

    Fixed gschem image gen and included the scm script

---
+++ resources/image.scm
@@ -0,0 +1,21 @@
+;; $Id$
+;;
+;; This file may be used to produce png files from gschem schematics from the
+;; command line.  Typical usage is:
+;;
+;;   gschem -p -o mysch.png -s /path/to/this/file/image.scm mysch.sch
+;;
+;; The schematic in "mysch.sch" will be exported to the file "mysch.png"
+
+(image-size 1500 1200)
+;(image-size 3200 2400)
+(image-color "enabled")
+;(image-color "disabled")
+
+; You need call this after you call any rc file function
+(gschem-use-rc-values)
+
+; filename is specified on the command line
+(gschem-image "dummyfilename")
+
+(gschem-exit)
